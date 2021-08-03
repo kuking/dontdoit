e3d6106044aec8952c294b9646f288de4345c9d9
Eduardo ES Riccardi <ed@kukino.uk>
2021-08-03 16:18:20 +0100

    progress

--- done.go
+++ done.go
@@ -6,18 +6,24 @@ type Done struct {
 	ONE          *big.Int
 	THREE        *big.Int
 	tmp          *big.Int
+	tmpAdd       *big.Int
 	provenUpTo   *big.Int
 	sparseProven []*big.Int
 }
 
 func NewDone(size int) *Done {
-	return &Done{
+	done := Done{
 		ONE:          big.NewInt(1),
 		THREE:        big.NewInt(3),
 		tmp:          big.NewInt(0),
+		tmpAdd:       big.NewInt(0),
 		provenUpTo:   big.NewInt(1),
 		sparseProven: make([]*big.Int, size),
 	}
+	for i := 0; i < size; i++ {
+		done.sparseProven[i] = big.NewInt(1)
+	}
+	return &done
 }
 
 func (d *Done) KnownToConverge(i *big.Int) bool {
@@ -25,27 +31,60 @@ func (d *Done) KnownToConverge(i *big.Int) bool {
 }
 
 func (d *Done) AddKnownToConverge(i *big.Int) {
-	d.tmp.Sub(i, d.provenUpTo)
-	if d.tmp.IsUint64() && d.tmp.Uint64() == 1 {
+	d.tmpAdd.Set(i)
+	d.tmpAdd.Sub(d.tmp, d.provenUpTo)
+	if d.tmpAdd.IsInt64() && d.tmpAdd.Int64() == 1 {
 		d.provenUpTo.Add(d.provenUpTo, d.ONE)
 	} else {
-		panic("implement sparseProven")
+		d.insertIntoSparseProven(i)
 	}
+	d.pumpProvenUpToIfPossible()
+}
+
+func (d *Done) insertIntoSparseProven(i *big.Int) {
+	for idx := 0; idx < len(d.sparseProven); idx++ {
+		cmp := d.sparseProven[idx].Cmp(i)
+		if cmp == 0 {
+			return // already there
+		} else if cmp > 0 || d.sparseProven[idx].Cmp(d.ONE) == 0 {
+			for idx2 := len(d.sparseProven) - 2; idx2 >= idx; idx2-- {
+				d.sparseProven[idx2+1].Set(d.sparseProven[idx2])
+			}
+			d.sparseProven[idx].Set(i)
+			return
+		}
+	}
+}
+
+func (d *Done) String() (out string) {
+	out = "ProvenUpTo: " + d.provenUpTo.Text(10) + " SparseProven: "
+	for i := 0; i < len(d.sparseProven); i++ {
+		out = out + d.sparseProven[i].Text(10) + " "
+		if d.sparseProven[i].Cmp(d.ONE) == 0 {
+			break
+		}
+	}
+	return
+}
+
+func (d *Done) pumpProvenUpToIfPossible() {
+
 }
 
 func (d *Done) VerifyConverges(i *big.Int, adding bool) bool {
 	if adding {
-		d.AddKnownToConverge(i)
+		defer d.AddKnownToConverge(i)
 	}
 	var loops int64 = 0
 	d.tmp.Set(i)
-	defer println()
 	for {
-		print(d.tmp.Text(10), " ")
+		//print(d.tmp.Text(10), " ")
 		if d.KnownToConverge(d.tmp) {
 			return true
 		}
-
+		if adding {
+			d.AddKnownToConverge(d.tmp)
+		}
 		if d.tmp.Bit(0) == 0 {
 			d.tmp.Rsh(d.tmp, 1) // divide by 2
 		} else {
---
+++ done_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestDoneSimplest(t *testing.T) {
+	d := NewDone(10)
+	if !d.KnownToConverge(d.ONE) {
+		t.Fail()
+	}
+	d.tmp.Set(d.ONE)
+	for i := 0; i < 1000; i++ {
+		d.tmp = d.tmp.Add(d.tmp, d.ONE)
+		if d.KnownToConverge(d.tmp) {
+			t.Fail()
+		}
+		d.AddKnownToConverge(d.tmp)
+		if !d.KnownToConverge(d.tmp) {
+			t.Fail()
+		}
+	}
+}
+
+func TestDoneStateFor10(t *testing.T) {
+	d := NewDone(10)
+	// 10, 5, 16, 8, 4, 2, 1
+	d.VerifyConverges(big.NewInt(10), true)
+	// provenUpTo: 2, sparseProven: 4, 5, 8, 10, 16
+	//println(d.String())
+	if d.provenUpTo.Int64() != 2 {
+		t.Fail()
+	}
+	if d.sparseProven[0].Int64() != 4 ||
+		d.sparseProven[1].Int64() != 5 ||
+		d.sparseProven[2].Int64() != 8 ||
+		d.sparseProven[3].Int64() != 10 ||
+		d.sparseProven[4].Int64() != 16 {
+		t.Fail()
+	}
+}
+
+func TestDoneStateFor3(t *testing.T) {
+	d := NewDone(10)
+	d.VerifyConverges(big.NewInt(3), true)
+	// provenUpTo: 5, sparseProven: 8, 10, 16
+	println(d.String())
+	if d.provenUpTo.Int64() != 5 {
+		t.Fail()
+	}
+	if d.sparseProven[0].Int64() != 8 ||
+		d.sparseProven[1].Int64() != 10 ||
+		d.sparseProven[4].Int64() != 16 {
+		t.Fail()
+	}
+
+}
